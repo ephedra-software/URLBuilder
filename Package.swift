d10801f488508c6c9d8ea47c1181c283af7c5f3d
Kirill Saltykov <51973198+KirillSaltykov@users.noreply.github.com>
2019-07-21 21:40:34 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "URLBuilder",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "URLBuilder",
             targets: ["URLBuilder"]),
