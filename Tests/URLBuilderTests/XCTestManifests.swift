e0eeaa44f21e6ce0af1e504b86b85188967611d7
Puasonych <basargin.erik@gmail.com>
2019-07-22 10:19:39 +0500

    Update tests for linux

--- Tests/URLBuilderTests/XCTestManifests.swift
+++ Tests/URLBuilderTests/XCTestManifests.swift
@@ -5,7 +5,14 @@ public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(URLBuilderTests.allTests),
         testCase(URLComponentsTests.allTests),
-        testCase(URLBuilderSchemeFunctionalityTests.allTests)
+        testCase(URLBuilderSchemeFunctionalityTests.allTests),
+        testCase(URLBuilderUserFunctionalityTests.allTests),
+        testCase(URLBuilderPasswordFunctionalityTests.allTests),
+        testCase(URLBuilderHostFunctionalityTests.allTests),
+        testCase(URLBuilderPortFunctionalityTests.allTests),
+        testCase(URLBuilderPathFunctionalityTests.allTests),
+        testCase(URLBuilderQueryFunctionalityTests.allTests),
+        testCase(URLBuilderFragmentFunctionalityTests.allTests)
     ]
 }
 #endif
